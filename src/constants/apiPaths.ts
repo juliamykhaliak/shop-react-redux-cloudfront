3ce6109ec936b1395acdb7d75565d255fa932e86
Yuliia Mykhaliak <yuliia_mykhaliak@epam.com>
2025-04-27 22:47:00 +0200

    Task 3: Use Lambda grtProducts, getProductById

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://n7mxcvv0wb.execute-api.us-east-1.amazonaws.com/prod/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
